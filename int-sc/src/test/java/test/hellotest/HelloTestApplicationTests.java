b411b4a4e76457d5bc79262f161d5eb7f22f2c4d
rkepdls <rkepdls@naver.com>
2023-07-10 17:58:40 +0900

    test
    색 변경 이슈 테스트

---
+++ int-sc/src/test/java/test/hellotest/HelloTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.hellotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
