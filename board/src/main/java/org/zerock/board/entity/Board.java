45fbc629df9b4554a0c9ea80132a4ec2a852d244
rkepdls <rkepdls@naver.com>
2023-07-19 18:00:28 +0900

    board project 시작
    테스트중 오류생긴상태 - 잡아야함 (255)

---
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,25 @@
+package org.zerock.board.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne (fetch = FetchType.LAZY) // 명시적으로 Lazy 로딩 지정
+    private Member writer;
+}
