34b817b126d9ee12d533ad6b31f6edaa7f59cd02
Gaden H <rkepdls@naver.com>
2023-07-31 21:39:30 +0900

    [중간 저장]
    집pc에서 리뷰 작성, 평점 평균 적용 확인
    1. test 파일 돌려서 리뷰어 등록 (데이터 100개)
    2. mid값은 첫번째 컬럼 숫자등록
    3. 별점 + 내용입력 후 등록하면 새로고침 되면서 콘솔에 내용 사라짐

--- mreview/src/main/java/org/zerock/mreview/controller/ReviewController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/ReviewController.java
@@ -1,6 +1,5 @@
 package org.zerock.mreview.controller;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -20,13 +19,23 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @GetMapping("/{mno}/all")
-    public ResponseEntity<List<ReviewDTO>> getList(@PathVariable("mno") Long mno) {
-        log.info("--------list--------");
-        log.info("MNO: "+mno);
+    public ResponseEntity<List<ReviewDTO>> getList(@PathVariable("mno") Long mno){
+        log.info("--------------list---------------");
+        log.info("MNO: " + mno);
 
         List<ReviewDTO> reviewDTOList = reviewService.getListOfMovie(mno);
 
         return new ResponseEntity<>(reviewDTOList, HttpStatus.OK);
+    }
+
+    @PostMapping("/{mno}")
+    public ResponseEntity<Long> addReview(@RequestBody ReviewDTO movieReviewDTO){
+        log.info("--------------add MovieReview---------------");
+        log.info("reviewDTO: " + movieReviewDTO);
+
+        Long reviewnum = reviewService.register(movieReviewDTO);
+
+        return new ResponseEntity<>( reviewnum, HttpStatus.OK);
     }
 
     @PutMapping("/{mno}/{reviewnum}")
@@ -49,4 +58,6 @@ public class ReviewController {
 
         return new ResponseEntity<>( reviewnum, HttpStatus.OK);
     }
+
 }
+
